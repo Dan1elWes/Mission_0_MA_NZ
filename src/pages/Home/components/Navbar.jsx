765e7eaf43862fb743f66f6b042d1475b6804c49
Daniel <danielw@missionreadyhq.com>
2024-11-08 07:29:49 +1300

    created navbar and layed out the 'menus' and added login button that is non functioning

--- src/pages/Home/components/Navbar.jsx
+++ src/pages/Home/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <p>Navbar</p>
-    </div>
-  )
+    <nav className={styles.navbar}>
+      <div className={styles.navbarLogo}>My Company</div>
+      <div className={styles.navbarLinks}>
+        <p>Menu1</p>
+        <p>Menu2</p>
+        <p>Menu3</p>
+        <button className={styles.loginBtn}>Login</button>
+      </div>
+    </nav>
+  );
 }
