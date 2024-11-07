6e2d8d99229fa634ee554db1231a379125e56ce9
Daniel <danielw@missionreadyhq.com>
2024-11-08 08:44:26 +1300

    downloaded free image for background of top half of the page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import styles from './Home.module.css'
-import Navbar from './components/Navbar'
+import React from "react";
+import styles from "./Home.module.css";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <div className={styles.homePage}>
+        <h1>Lorem ipsum dolor sit amet</h1>
+      </div>
     </div>
-  )
+  );
 }
